94621711711e20d355d2064e2da6a8323cf944c6
Kanak Agarwal <kanak5march@gmail.com>
2024-04-21 10:59:29 +0530

    utils code update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-// import { app } from "./app.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at port :${process.env.PORT}`);
+    })
+  )
+  .catch((error) => {
+    console.log("MONGO DB connection failed !!!", error);
+    throw error;
+  });
+
 /*
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
